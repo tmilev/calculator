12ff400976dd5f877de314d96dcc6811933341af
tmilev <tmilev@users.noreply.github.com>
2010-02-19 13:09:36 +0000

    i almlost lost my work... wth ?

--- vp_cgi_interface.cpp
+++ vp_cgi_interface.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "polyhedra.h"
+
+extern int GlobalPointerCounter;
+
 void getPath(char* path, std::string& output)
 { int length=0;
   if (path==0) return;
@@ -26,44 +29,48 @@ int main(int argc, char **argv)
   //std::cout <<  "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">"
 	//					<< std::endl;
   //std::cout << "<html lang=\"en\" dir=\"LTR\">"<< std::endl;
-  std::string tempS;
-  getPath(argv[0],tempS);
+  std::string tempS2, tempS1;
+  getPath(argv[0],tempS1);
   std::stringstream out;
-  out<<tempS;
- // out<<"../vector_partition.bmp";
-  tempS=out.str();
-//  std::fstream tempf;
- // ::CGIspecificRoutines::MakeABitmap(tempS,tempf);
+  out<<tempS1;
   out<<"../vector_partition.html";
 
-  tempS=out.str();
+  tempS1=out.str();
   std::fstream fileHeaderHtml;
-  rootFKFTcomputation::OpenDataFile(fileHeaderHtml,tempS);
+  rootFKFTcomputation::OpenDataFile(fileHeaderHtml,tempS1);
   char buffer[1024];
   while (!fileHeaderHtml.eof())
   { fileHeaderHtml.read(buffer,1024);
     std::cout.write(buffer, fileHeaderHtml.gcount());
   }
-  WeylGroup tempWeyl;
-  tempWeyl.MakeArbitrary('A',3);
-  tempWeyl.ComputeRho();
-  std::cout<<"<script type=\"text/javascript\">\n";
-  std::cout<< "\tvar rootsArray= new Array(" <<tempWeyl.RootsOfBorel.size << ");";
-  for (int i=0;i<tempWeyl.RootsOfBorel.size;i++)
-  { std::cout<< "\n\trootsArray["<<i<<"]= new Array(3);\n";
-    for (int j=0;j<3;j++)
-    { tempWeyl.RootsOfBorel.TheObjects[i].TheObjects[j].ElementToString(tempS);
-      std::cout<< "\trootsArray["<<i<< "]["<<j <<"]="<<tempS<<";";
-    }
-  }
-	std::cout<<"\n\tgeneratePageFromDimAndNum(3,6,5);\n</script>\n";
-  std::cin >> tempS;
-	//std::cout<< tempS;
-  if(::CGIspecificRoutines::ReadDataFromCGIinput(tempS, theComputationSetup))
+  std::cin >> tempS1;
+  if(::CGIspecificRoutines::ReadDataFromCGIinput(tempS1, theComputationSetup))
   {	theComputationSetup.Run();
 		::CGIspecificRoutines::MakeReportFromComputationSetup(theComputationSetup);
   }
-	std::cout<<"</BODY>\n</HTML>";
+  else
+  {	WeylGroup tempWeyl;
+		tempWeyl.MakeArbitrary('B',3);
+		tempWeyl.ComputeRho();
+		theComputationSetup.VPVectors.CopyFromBase(tempWeyl.RootsOfBorel);
+		theComputationSetup.theChambers.AmbientDimension=3;
+	//	std::cout<<"asdfadsfasdfasdf";
+  }
+  std::cout<<"<script type=\"text/javascript\">\n";
+  std::cout<< "\tvar rootsArray= new Array(" <<theComputationSetup.VPVectors.size << ");";
+  for (int i=0;i<theComputationSetup.VPVectors.size;i++)
+  { std::cout	<< "\n\trootsArray["<<i<<"]= new Array("
+							<< theComputationSetup.theChambers.AmbientDimension<<");\n";
+    for (int j=0;j<theComputationSetup.theChambers.AmbientDimension;j++)
+    { theComputationSetup.VPVectors.TheObjects[i].TheObjects[j].ElementToString(tempS2);
+      std::cout<< "\trootsArray["<<i<< "]["<<j <<"]="<<tempS2<<";";
+    }
+  }
+	std::cout	<<"\n\tgeneratePageFromDimAndNum("
+						<<theComputationSetup.theChambers.AmbientDimension<<","
+						<<theComputationSetup.VPVectors.size <<","<<-1<<");\n</script>\n";
+	//std::cout<<ParallelComputing::GlobalPointerCounter<<tempS1;
+  std::cout<<"</BODY>\n</HTML>";
   return 0;   // To avoid Apache errors.
 }
 

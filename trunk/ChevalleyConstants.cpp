cb60542b40abb00c4a2d0b13086070a13cd1a84a
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2010-04-17 22:43:29 +0000

    first debuggable version of chevalley constants, pops on an assert.

--- trunk/ChevalleyConstants.cpp
+++ trunk/ChevalleyConstants.cpp
@@ -5,21 +5,32 @@ void SimpleLieAlgebra::ComputeChevalleyConstants
   (char WeylLetter, int WeylIndex, GlobalVariables& theGlobalVariables)
 { this->theWeyl.MakeArbitrary(WeylLetter, WeylIndex);
   this->theWeyl.ComputeRho();
-  this->Computed.NullifyAll(false);
   this->ChevalleyConstants.init(this->theWeyl.RootSystem.size, this->theWeyl.RootSystem.size);
+  this->Computed.init(this->theWeyl.RootSystem.size, this->theWeyl.RootSystem.size);
+  this->Computed.NullifyAll(false);
   this->ChevalleyConstants.NullifyAll();
   Selection nonExploredRoots;
+  this->flagAnErrorHasOccurredTimeToPanic=true;
   roots& posRoots=this->theWeyl.RootsOfBorel;
   nonExploredRoots.init(posRoots.size);
   for (int i=0;i<posRoots.size;i++)
     nonExploredRoots.AddSelectionAppendNewIndex(i);
+  for (int i=0;i<this->theWeyl.RootSystem.size;i++)
+		for(int j=i+1;j<this->theWeyl.RootSystem.size;j++)
+			if(!this->theWeyl.IsARoot(this->theWeyl.RootSystem.TheObjects[i]+this->theWeyl.RootSystem.TheObjects[j]))
+			{ this->Computed.elements[i][j]=true;
+				this->ChevalleyConstants.elements[i][j].MakeZero();
+				this->Computed.elements[j][i]=true;
+				this->ChevalleyConstants.elements[j][i].MakeZero();
+			}
   Rational tempRat;
   while (nonExploredRoots.CardinalitySelection>0)
-  { int theBorelIndex=nonExploredRoots.elements[0];
+  { this->ComputeDebugString();
+		int theBorelIndex=nonExploredRoots.elements[0];
     Rational theHeight= posRoots.TheObjects[theBorelIndex].GetHeight();
     for (int i=1;i<nonExploredRoots.CardinalitySelection;i++)
     { posRoots.TheObjects[nonExploredRoots.elements[i]].GetHeight(tempRat);
-      if (tempRat.IsGreaterThan(theHeight))
+      if (theHeight.IsGreaterThan(tempRat))
       { theHeight.Assign(tempRat);
         theBorelIndex=nonExploredRoots.elements[i];
       }
@@ -33,6 +44,7 @@ void SimpleLieAlgebra::ComputeChevalleyConstants
     { root& smallRoot1=this->theWeyl.RootSystem.TheObjects[i];
       smallRoot1.GetHeight(tempRat);
       tempRat.AssignAbsoluteValue();
+      int indexMinusRoot=this->theWeyl.RootSystem.IndexOfObjectHash(-smallRoot1);
       if (theHeight.IsGreaterThan(tempRat))
       { smallRoot2=theRoot-smallRoot1;
         int indexSmallRoot2=this->theWeyl.RootSystem.IndexOfObjectHash(smallRoot2);
@@ -40,17 +52,19 @@ void SimpleLieAlgebra::ComputeChevalleyConstants
         { if (FirstIndexFirstChoice==-1)
           { FirstIndexFirstChoice=i;
             SecondIndexFirstChoice=indexSmallRoot2;
-            this->ChevalleyConstants.elements[FirstIndexFirstChoice][SecondIndexFirstChoice].MakeOne();
+            this->ChevalleyConstants.elements[indexMinusRoot][theIndex].MakeOne();
           }
-          else
-          { this->ComputeOneChevalleyConstant
-              (FirstIndexFirstChoice, SecondIndexFirstChoice, i, theIndex);
-          }
-          this->ExploitSymmetryChevalleyConstants(i,theIndex);
+          this->ComputeOneChevalleyConstant
+						(FirstIndexFirstChoice, SecondIndexFirstChoice, indexSmallRoot2, theIndex);
+          this->ComputeDebugString();
+          this->ExploitSymmetryChevalleyConstants(indexSmallRoot2,theIndex);
         }
       }
+      if (this->flagAnErrorHasOccurredTimeToPanic)
+				this->ComputeDebugString();
     }
-
+    nonExploredRoots.selected[theIndex]=false;
+    nonExploredRoots.ComputeIndicesFromSelection();
   }
 }
 
@@ -64,6 +78,8 @@ void SimpleLieAlgebra::ExploitSymmetryChevalleyConstants
   this->ChevalleyConstants.elements[indexEta][indexMinusEpsilon].Assign
     (this->ChevalleyConstants.elements[indexMinusEpsilon][indexEta]);
   this->ChevalleyConstants.elements[indexEta][indexMinusEpsilon].Minus();
+  this->ComputeDebugString();
+  this->Computed.elements[indexEta][indexMinusEpsilon]=true;
   root tempRoot;
   tempRoot.Assign(this->theWeyl.RootSystem.TheObjects[indexEta]);
   tempRoot.Add(this->theWeyl.RootSystem.TheObjects[indexMinusEpsilon]);
@@ -75,9 +91,13 @@ void SimpleLieAlgebra::ExploitSymmetryChevalleyConstants
   this->ChevalleyConstants.elements[indexEpsilon][indexMinusEta].AssignInteger(-i*i);
   this->ChevalleyConstants.elements[indexEpsilon][indexMinusEta].DivideBy
     (this->ChevalleyConstants.elements[indexMinusEpsilon][indexEta]);
+  this->Computed.elements[indexEpsilon][indexMinusEta]=true;
+  this->ComputeDebugString();
   this->ChevalleyConstants.elements[indexMinusEta][indexEpsilon].Assign
     (this->ChevalleyConstants.elements[indexEpsilon][indexMinusEta]);
   this->ChevalleyConstants.elements[indexMinusEta][indexEpsilon].Minus();
+  this->Computed.elements[indexMinusEta][indexEpsilon]=true;
+  this->ComputeDebugString();
 }
 
 void SimpleLieAlgebra::ComputeOneChevalleyConstant
@@ -105,4 +125,13 @@ void SimpleLieAlgebra::ComputeOneChevalleyConstant
       this->ChevalleyConstants.elements[indexDelta][indexGammaMinusEpsilon])/
         this->ChevalleyConstants.elements[indexGamma][indexDelta];
   this->ChevalleyConstants.elements[indexMinusEpsilon][indexEta].Minus();
+  this->Computed.elements[indexMinusEpsilon][indexEta]=true;
+}
+
+void ::SimpleLieAlgebra::ElementToString(std::string &output)
+{ std::stringstream out;
+	std::string tempS;
+	this->ChevalleyConstants.ElementToSting(tempS);
+	out << tempS;
+	output=out.str();
 }

661fdb38c44ee4fff03bc04e3beb9ccc35f7bf2a
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2010-05-22 17:54:13 +0000

    The file is to be used from now on in place of the old example.cpp

---
+++ trunk/custom_code_here.cpp
@@ -0,0 +1,242 @@
+#include "polyhedra.h"
+// This file is meant to be for people to modify if they do not want to modify the main files polyhedra.cpp or polyhedra.h
+// The reason I would recommend that is because the file polyhedra.cpp compiles very slowly (around 30 seconds), so small modifications
+// take a long time to check. Using a separate file and linking it to the GUI is the solution I chose (may not be the best, see below for discussion).
+// If the code you write here reaches the mature phase where you have realized all functions you think it should have
+// and you are generally satisfied with it, simply cut & paste it in the main files (the clas declarations in polyhedra.h and the implementation in polyhedra.cpp).
+
+//Discussion: other options for speeding up the compilation that I have considered.
+//1. Cut up the main files polyhedra.h and polyhedra.cpp into small quick-to-compile pieces. This might be the best solution in the long run. However, I do not want to
+//   do it just yet, because 1) I am not sure what should those pieces be - it is not yet clear in my head how to partition the code in conceptually distinct pieces
+//   2) this would certainly create additional maintainance work 3) this will increase the learning curve for a person wanting to just use the program and wanting to eventually
+//   modify some tiny bit of it and 4) I got an advice on the c++ forum I go to, www.cplusplus.com, that partitioning the .h file will eventually lead to slower compile times
+//   on all pieces of code that use it, especially with the massive use of templates that I do. Therefore, such a partitioning should not be done before the code reaches
+//   greater maturity (see also point (1) ).
+//2. Use precompiled headers or some other voodoo. I am tocally against that. Those are compiler specific, will require me to learn extra unnecessary info which might
+//    be out of date in a few years, and will make even higher entry learning curve for another to join the project. This is bad.
+//    I should mention in favor of Microsoft that their IDE does recompile very quickly small modifications of the file polyhedra.cpp. I believe it does so by
+//    keeping a database of your recently changed code, and recompiling only the recently changed pieces. Hats off to Microsoft for doing that completely programmatically,
+//    and not bothering the programmer with stupid things such as how to set up precompiled headers.
+//
+// To whomever might be reading this (if anyone): happy hacking and I hope you find my code useful, that it didn't cause you many headaches, and that you
+// did something useful with it! Cheers!
+
+class ElementWeylAlgebra
+{
+private:
+  PolynomialRationalCoeff StandardOrder;
+public:
+  std::string DebugString;
+  void ComputeDebugString(){this->ElementToString(this->DebugString, true);};
+	void ElementToString(std::string& output, ListBasicObjects<std::string>& alphabet);
+	void ElementToString(std::string& output, bool useXYs);
+  int NumVariables;
+  void MakeGEpsPlusEps(int i, int j, int NumVars);
+  void MakeGEpsMinusEps(int i, int j, int NumVars);
+  void MakeGMinusEpsMinusEps(int i, int j, int NumVars);
+  void Nullify(int NumVars);
+  void MultiplyOnTheLeft(ElementWeylAlgebra& standsOnTheLeft, GlobalVariables& theGlobalVariables);
+  void LieBracketOnTheLeft(ElementWeylAlgebra& standsOnTheLeft, GlobalVariables& theGlobalVariables);
+  void Assign(const ElementWeylAlgebra& other)
+  { this->StandardOrder.Assign(other.StandardOrder);
+    this->NumVariables=other.NumVariables;
+  };
+  void Subtract(ElementWeylAlgebra& other)
+  { this->StandardOrder.Subtract(other.StandardOrder);
+  };
+  void MultiplyTwoMonomials( Monomial<Rational>& left, Monomial<Rational>& right, PolynomialRationalCoeff& OrderedOutput, GlobalVariables& theGlobalVariables);
+  ElementWeylAlgebra()
+  { this->NumVariables=0; };
+};
+
+void ElementWeylAlgebra::MultiplyTwoMonomials( Monomial<Rational>& left, Monomial<Rational>& right, PolynomialRationalCoeff& OrderedOutput, GlobalVariables& theGlobalVariables)
+{ Monomial<Rational> buffer;
+  SelectionWithDifferentMaxMultiplicities& tempSel=theGlobalVariables.selWeylAlgebra1;
+  assert(left.NumVariables%2==0);
+  int theDimension=left.NumVariables/2;
+  tempSel.Multiplicities.initFillInObject(theDimension, 0);
+  tempSel.MaxMultiplicities.SetSizeExpandOnTopNoObjectInit(theDimension);
+  for (int i=0; i<theDimension; i++)
+    tempSel.MaxMultiplicities.TheObjects[i]=left.degrees[theDimension+i];
+  tempSel.elements.initFillInObject(theDimension,0);
+  buffer.init(left.NumVariables);
+  assert(left.NumVariables==right.NumVariables);
+  OrderedOutput.Nullify(left.NumVariables);
+  int numCycles= tempSel.getTotalNumSubsets();
+  for (int i=0; i<numCycles; i++)
+  { buffer.Coefficient.MakeOne();
+    for (int k=0; k< theDimension; k++)
+    { int multDrop=tempSel.Multiplicities.TheObjects[k];
+      buffer.Coefficient.MultiplyBy(Rational::NChooseK(left.degrees[theDimension+k], multDrop)*Rational::NChooseK(right.degrees[k], multDrop)* Rational::Factorial(multDrop));
+      buffer.degrees[k]=left.degrees[k]+right.degrees[k]-multDrop;
+      buffer.degrees[k+theDimension]= left.degrees[k+theDimension]+right.degrees[k+theDimension]-multDrop;
+    }
+    OrderedOutput.AddMonomial(buffer);
+    tempSel.IncrementSubset();
+  }
+}
+
+void ElementWeylAlgebra::LieBracketOnTheLeft(ElementWeylAlgebra& standsOnTheLeft, GlobalVariables& theGlobalVariables)
+{ ElementWeylAlgebra tempEl1, tempEl2;
+  tempEl1.Assign(*this);
+  tempEl1.MultiplyOnTheLeft(standsOnTheLeft, theGlobalVariables);
+  tempEl2.Assign(standsOnTheLeft);
+  tempEl2.MultiplyOnTheLeft(*this, theGlobalVariables);
+  this->Assign(tempEl1);
+  this->Subtract(tempEl2);
+}
+
+void ElementWeylAlgebra::MultiplyOnTheLeft(ElementWeylAlgebra& standsOnTheLeft, GlobalVariables& theGlobalVariables)
+{ PolynomialRationalCoeff buffer;
+  PolynomialRationalCoeff Accum;
+  Accum.Nullify(this->NumVariables*2);
+  for (int j=0; j<standsOnTheLeft.StandardOrder.size; j++)
+    for (int i=0; i<this->StandardOrder.size; i++)
+    { this->MultiplyTwoMonomials(standsOnTheLeft.StandardOrder.TheObjects[j], this->StandardOrder.TheObjects[i], buffer, theGlobalVariables);
+      Accum.AddPolynomial(buffer);
+    }
+  this->StandardOrder.Assign(Accum);
+}
+
+void ElementWeylAlgebra::ElementToString(std::string& output, ListBasicObjects<std::string>& alphabet)
+{ std::stringstream out;
+  std::string tempS;
+  for (int i=0; i<this->StandardOrder.size; i++)
+  { this->StandardOrder.TheObjects[i].Coefficient.ElementToString(tempS);
+    bool hasMinus=(tempS[0]=='-');
+    if (this->StandardOrder.TheObjects[i].TotalDegree()!=0)
+    { if (tempS=="1")
+        tempS="";
+      if (tempS=="-1")
+        tempS="-";
+    }
+    if (!hasMinus && i!=0)
+      out<<'+';
+    out << tempS;
+    for (int k=0; k<this->NumVariables; k++)
+      if (this->StandardOrder.TheObjects[i].degrees[k]!=0)
+      { out <<"{"<<alphabet.TheObjects[k]<<"}";
+        int tempI=(int) this->StandardOrder.TheObjects[i].degrees[k];
+        if (tempI!=1)
+          out <<"^{"<< tempI<<"}";
+      }
+    for (int k=0; k<this->NumVariables; k++)
+      if (this->StandardOrder.TheObjects[i].degrees[this->NumVariables+ k]!=0)
+      { out <<"\\partial_{"<<alphabet.TheObjects[k]<<"}";
+        int tempI=this->StandardOrder.TheObjects[i].degrees[this->NumVariables+k];
+        if (tempI!=1)
+          out <<"^{" <<tempI<<"}";
+      }
+  }
+  output=out.str();
+}
+
+void ElementWeylAlgebra::ElementToString(std::string& output, bool useXYs)
+{ ListBasicObjects<std::string> alphabet;
+  alphabet.SetSizeExpandOnTopNoObjectInit(this->NumVariables);
+  int numCycles=this->NumVariables;
+  if (useXYs)
+  { assert(this->NumVariables%2==0);
+    numCycles= this->NumVariables/2;
+  }
+  for (int i=0; i<numCycles; i++)
+  { std::stringstream out, out2;
+    out << "x_"<<i+1;
+    alphabet.TheObjects[i]= out.str();
+    if (useXYs)
+    { out2<< "y_"<< i+1;
+      alphabet.TheObjects[i+numCycles]= out2.str();
+    }
+  }
+  this->ElementToString(output, alphabet);
+}
+
+void ElementWeylAlgebra::MakeGEpsPlusEps(int i, int j, int NumVars)
+{ this->Nullify(NumVars*2);
+  Monomial<Rational> tempMon;
+  tempMon.Coefficient.MakeOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[i]=1;
+  tempMon.degrees[j+this->NumVariables+NumVars]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+
+  tempMon.Coefficient.MakeMOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[j]=1;
+  tempMon.degrees[i+this->NumVariables+NumVars]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+}
+
+void ElementWeylAlgebra::MakeGEpsMinusEps(int i, int j, int NumVars)
+{ this->Nullify(NumVars*2);
+  Monomial<Rational> tempMon;
+  tempMon.Coefficient.MakeOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[i]=1;
+  tempMon.degrees[j+this->NumVariables]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+
+  tempMon.Coefficient.MakeMOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[j+NumVars]=1;
+  tempMon.degrees[i+this->NumVariables+NumVars]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+}
+
+void ElementWeylAlgebra::MakeGMinusEpsMinusEps(int i, int j, int NumVars)
+{ this->Nullify(NumVars*2);
+  Monomial<Rational> tempMon;
+  tempMon.Coefficient.MakeOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[i+NumVars]=1;
+  tempMon.degrees[j+this->NumVariables]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+
+  tempMon.Coefficient.MakeMOne();
+  tempMon.MakeConstantMonomial(this->NumVariables*2, tempMon.Coefficient);
+  tempMon.degrees[j+NumVars]=1;
+  tempMon.degrees[i+this->NumVariables]=1;
+  this->StandardOrder.AddMonomial(tempMon);
+}
+
+void ElementWeylAlgebra::Nullify(int NumVars)
+{ this->NumVariables=NumVars;
+  this->StandardOrder.Nullify (this->NumVariables*2);
+}
+
+void main_test_function(std::string& output, GlobalVariables& theGlobalVariables)
+{ std::stringstream out;
+  ElementWeylAlgebra theElement, tempEl1, tempEl2, tempEl3, tempEl4;
+  tempEl1.MakeGEpsPlusEps(0,1, 4);
+  tempEl1.ComputeDebugString();
+  out <<"\\documentclass{article}\n\\begin{document}";
+  out<<"\n\\begin{eqnarray*}\ng^{\\varepsilon_1+\\varepsilon_2}= ";
+  out <<tempEl1.DebugString;
+  out <<"\n\\end{eqnarray*}\n";
+  tempEl2.MakeGEpsMinusEps(0,1,4);
+  tempEl2.ComputeDebugString();
+  out <<"\\begin{eqnarray*}\ng^{\\varepsilon_1-\\varepsilon_2}= ";
+  out <<tempEl2.DebugString;
+  out <<"\n\\end{eqnarray*}\n";
+  tempEl3.MakeGMinusEpsMinusEps(0,1,4);
+  tempEl3.ComputeDebugString();
+  out <<"\\begin{eqnarray*}\ng^{-\\varepsilon_1-\\varepsilon_2}= ";
+  out <<tempEl3.DebugString;
+  out <<"\n\\end{eqnarray*}\n";
+  tempEl4.Assign(tempEl3);
+  tempEl4.MultiplyOnTheLeft(tempEl1,theGlobalVariables);
+  tempEl4.ComputeDebugString();
+  out <<"\\begin{eqnarray*}\ng^{\\varepsilon_1+\\varepsilon_2}g^{-\\varepsilon_1-\\varepsilon_2}= ";
+  out <<tempEl4.DebugString;
+  out <<"\n\\end{eqnarray*}\n";
+
+  tempEl4.Assign(tempEl3);
+  tempEl4.LieBracketOnTheLeft(tempEl1,theGlobalVariables);
+  tempEl4.ComputeDebugString();
+  out <<"\\begin{eqnarray*}\n[g^{\\varepsilon_1+\\varepsilon_2},g^{-\\varepsilon_1-\\varepsilon_2}]= ";
+  out <<tempEl4.DebugString;
+  out <<"\n\\end{eqnarray*}\n";
+
+  out<<"\\end{document}";
+  output=out.str();
+}

24bc816e18d10b9eda073fe19b1610f1c29bb2aa
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2013-01-24 02:30:11 +0000

    Expression::ToString modification.

--- trunk/vpfCalculatorMain.cpp
+++ trunk/vpfCalculatorMain.cpp
@@ -492,7 +492,7 @@ GroebnerLexUpperLimit{}(1000, x_{6}x_{18}+2x_{5}x_{17}+x_{4}x_{16}+2x_{3}x_{15}+
 //  civilizedInput="g:=SemisimpleLieAlgebra{}G_2; g_1;";
 //  civilizedInput="Polynomial{}(a+b+c)";
 //civilizedInput="printSemisimpleLieAlgebra{}(g_2);";
-civilizedInput="v_\\lambda:=hwv{}(G_2, (1,0),(0,0));";
+//civilizedInput="v_\\lambda:=hwv{}(G_2, (1,0),(0,0));";
   std::stringstream tempStreamXX;
   static_html4(tempStreamXX);
   std::cout << "<table>\n <tr valign=\"top\">\n <td>";

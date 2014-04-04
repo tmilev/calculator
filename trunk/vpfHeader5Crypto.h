2642e4bc9d31cb758dec2cc5cdd8ca6e434fd29c
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2014-04-04 14:03:42 +0000

    Crypto header, forgot to commit last time!

---
+++ trunk/vpfHeader5Crypto.h
@@ -0,0 +1,14 @@
+#ifndef header_crypto_was_already_defined
+#define header_crypto_was_already_defined
+#include "vpfHeader1General0_General.h"
+static ProjectInformationInstance projectInfoCryptoHeader(__FILE__, "Crypto class declaration.");
+
+class Crypto
+{
+public:
+  static std::string CharsToBase64String(const List<unsigned char>& input);
+  static bool StringBase64ToBitStream(const std::string& input, List<unsigned char>& output);
+  static bool Get6bitFromChar(char input, uint32_t& output);
+  static unsigned char GetCharFrom6bit(uint32_t input);
+};
+#endif // header_crypto_was_already_defined

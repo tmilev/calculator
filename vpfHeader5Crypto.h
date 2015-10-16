c972c1876a27e98a90d674964e22e7ec98c567e3
tmilev <tmilev@users.noreply.github.com>
2015-10-16 22:43:52 +0000

    Experimental sha1-implemenetation rough draft. NOT tested.

--- vpfHeader5Crypto.h
+++ vpfHeader5Crypto.h
@@ -10,5 +10,10 @@ public:
   static bool StringBase64ToBitStream(const std::string& input, List<unsigned char>& output, std::stringstream* comments=0);
   static bool Get6bitFromChar(char input, uint32_t& output);
   static unsigned char GetCharFrom6bit(uint32_t input);
+
+  static void convertUint32toBigendianStringAppendResult(uint32_t& input, std::string& outputAppend);
+  static void convertUint64toBigendianStringAppendResult(uint64_t& input, std::string& outputAppend);
+  static uint32_t leftRotateAsIfBigEndian(uint32_t input, int numBitsToRotate);
+  static void computeSha1(const std::string& inputString, List<uint32_t>& output);
 };
 #endif // header_crypto_was_already_defined

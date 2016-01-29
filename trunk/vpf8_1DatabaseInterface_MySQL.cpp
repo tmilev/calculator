079ec356cee65d8c4b4607526479d6b86d1da51c
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2016-01-29 02:03:41 +0000

    Added error stream redirection to system command execution.

--- trunk/vpf8_1DatabaseInterface_MySQL.cpp
+++ trunk/vpf8_1DatabaseInterface_MySQL.cpp
@@ -1062,10 +1062,9 @@ bool UserCalculator::SendActivationEmail(DatabaseRoutines& theRoutines, std::str
   << " todor.milev@gmail.com\n\n";
   emailStream << this->activationTokenUnsafe << "\n\nGood luck with our course, \n Your calculus instructors.";
   std::string emailLog=theGlobalVariables.CallSystemWithOutput(theEmailRoutines.GetCommandToSendEmailWithMailX());
-  stOutput << "Calling system with " << theEmailRoutines.GetCommandToSendEmailWithMailX()
-  << " to get output: \n" << emailLog;
-  comments << "\nEmail sending log:\n"
-  << emailLog;
+  stOutput << "Calling system with:<br>" << theEmailRoutines.GetCommandToSendEmailWithMailX()
+  << "<br>\n to get output: \n<br>" << emailLog;
+  comments << "\nEmail sending log:\n" << emailLog;
   return true;
 }
 

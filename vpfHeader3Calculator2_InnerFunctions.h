491772d4411553dc8b05839b2e32258347586d3e
tmilev <tmilev@users.noreply.github.com>
2016-03-16 07:21:39 +0000

    More bug fixes with calculator functions.

--- vpfHeader3Calculator2_InnerFunctions.h
+++ vpfHeader3Calculator2_InnerFunctions.h
@@ -39,6 +39,7 @@ public:
   static bool innerUrlStringToNormalString(Calculator& theCommands, const Expression& input, Expression& output);
   static bool innerURLKeyValuePairsToNormalRecursive(Calculator& theCommands, const Expression& input, Expression& output);
 
+  static bool innerExpressionToString(Calculator& theCommands, const Expression& input, Expression& output);
   static bool innerQuoteToString(Calculator& theCommands, const Expression& input, Expression& output);
   static bool innerBase64ToCharToBase64Test(Calculator& theCommands, const Expression& input, Expression& output);
   static bool innerCharToBase64(Calculator& theCommands, const Expression& input, Expression& output);

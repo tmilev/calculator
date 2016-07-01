15b9b7a6fc40ea5f91ab8d7e8ae4e9a0474b896a
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2016-07-01 15:43:51 +0000

    First working version of a sqrt mathquill button.

--- trunk/htmlsnippets/initializebuttons.js
+++ trunk/htmlsnippets/initializebuttons.js
@@ -1,9 +1,9 @@
 var answerIdsPureLatex;
-var answerIdsMathQuill;
+var answerMathQuillObjects=[];
 function initializeButtons(){
 for (i=0; i<answerIdsPureLatex.length; i++){ 
   newElt= document.createElement("span");
-  newElt.innerHTML="<button onclick='sqrtClick(answerIdsMathQuill);'>sqrt</button>";
+  newElt.innerHTML="<button onclick='sqrtClick(answerMathQuillObjects[" + i + "]);'>sqrt</button>";
   currentElt=document.getElementById(answerIdsPureLatex[i]);	
   currentElt.parentNode.insertBefore(newElt, currentElt);
 }
@@ -12,6 +12,6 @@ for (i=0; i<answerIdsPureLatex.length; i++){
 
 }
 
-function sqrtClick(mathQuillObject){ 
-  mathQuill
+function sqrtClick(currentMathField){ 
+  currentMathField.write("\\sqrt{}")
 }

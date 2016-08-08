3749a618951c908ee33467a442dbc35759b49489
tmilev <tmilev@users.noreply.github.com>
2016-08-08 19:36:15 +0000

    #include bug fix.

--- src/custom/vpfExamAndTeachingRoutines.cpp
+++ src/custom/vpfExamAndTeachingRoutines.cpp
@@ -1,6 +1,6 @@
 //The current file is licensed under the license terms found in the main header file "vpf.h".
 //For additional information refer to the file "vpf.h".
-#include "vpfHeader8HtmlExamAndTeachingCommon.h"
+#include "../vpfHeader8HtmlExamAndTeachingCommon.h"
 
 ProjectInformationInstance projectInfoInstanceWebServerExamAndTeachingRoutinesCustomCode
 (__FILE__, "Routines for calculus teaching: calculator exam mode. Shared code. ");

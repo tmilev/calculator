b61217f5f6cfb87a7137361ef312949fbd5a2e65
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2016-10-27 21:52:32 +0000

    Apache navigation bar fix, javascript fixes.

--- trunk/html-common/initializebuttons.js
+++ trunk/html-common/initializebuttons.js
@@ -3,6 +3,7 @@ var answerMathQuillObjects=[];
 var answerMQspanIds;
 var preferredButtonContainers;
 //var lastFocus;
+var charsToSplit=['x','y'];
 
 function processMathQuillLatex(theText)
 { for (var i=0; i<theText.length; i++)
@@ -10,6 +11,17 @@ function processMathQuillLatex(theText)
       if (theText[i]=='_' || theText[i]=='^' &&
           theText[i+1]!='{')
         theText=theText.slice(0,i+1)+'{'+theText[i+1]+'}'+theText.slice(i+2);
+  if (charsToSplit!=undefined)
+    for (var i=0; i<theText.length-1; i++)
+    { for (var j=0; j<charsToSplit.length; j++)
+        if (theText[i]==charsToSplit[j] &&
+            theText[i+1]!=' ' && theText[i+1]!='\\'
+            && theText[i+1]!='+' && theText[i+1]!='*'
+            && theText[i+1]!='/' && theText[i+1]!='-'
+            && theText[i+1]!='='
+            )
+          theText=theText.slice(0,i+1)+" "+theText.slice(i+1);
+    }
   return theText;
 }
 

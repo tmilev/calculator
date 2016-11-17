6aa0558fb56b8a5fa4ec68b4f9971c88498fab7e
tmilev <tmilev@3e39de04-88a4-4559-b743-a024344d6f26>
2016-11-17 06:06:13 +0000

    Deleting obsolete files.

--- trunk/src/old-code/vpfCalculatorMain.cpp
+++
@@ -1,9 +0,0 @@
-//The current file is licensed under the license terms found in the main header file "vpf.h".
-//For additional information refer to the file "vpf.h".
-#include "vpfHeader6WebServer.h"
-
-ProjectInformationInstance projectInfoInstanceCalculatorCpp(__FILE__, "Calculator main f-n.");
-
-int main(int argc, char **argv)
-{ return WebServer::main(argc, argv);
-}

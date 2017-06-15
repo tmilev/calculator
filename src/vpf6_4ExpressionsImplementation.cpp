0a4ac0a2d2b75b418f6d6ee5a6b1497e571d586b
Todor Milev <todor.milev@gmail.com>
2017-06-14 23:59:09 -0400

    GetFreeVariables now excludes and, or and not.

--- src/vpf6_4ExpressionsImplementation.cpp
+++ src/vpf6_4ExpressionsImplementation.cpp
@@ -1514,7 +1514,8 @@ bool Expression::GetFreeVariables(HashedList<Expression>& outputAccumulateFreeVa
       if (atomName=="=" || atomName==">" || atomName=="\"" || atomName=="==" ||
           atomName=="<" || atomName=="Sequence" || atomName=="Matrix" ||
           atomName=="\\cup" || atomName=="\\infty" || atomName=="IntervalClosed" ||
-          atomName=="[)" || atomName=="(]")
+          atomName=="[)" || atomName=="(]" || atomName=="and" || atomName=="or" ||
+          atomName=="not")
         doAddExpression=false;
     if (doAddExpression && excludeNamedConstants)
       if (this->owner->knownDoubleConstants.Contains(*this))

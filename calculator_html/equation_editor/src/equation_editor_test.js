615df2da7ca013d39def176851761399fe6968de
tmilev <todor.milev@gmail.com>
2023-04-29 08:26:17 -0700

    An end-to-end-ish test for the equation editor.

--- calculator_html/equation_editor/src/equation_editor_test.js
+++ calculator_html/equation_editor/src/equation_editor_test.js
@@ -1,3 +1,10 @@
-const editor = require("./equation_editor");
-
-console.log("Hi world");
+describe("Equation editor", function () {
+  it("Equation editor with basic content", async function () {
+    let container = document.createElement("div");
+    document.body.appendChild(container);
+    let equationEditor = new EquationEditor(container, null, null);
+    equationEditor.writeLatex("\\frac{1}{2}");
+    await 0;
+    expect(true).toBe(true);
+  });
+});

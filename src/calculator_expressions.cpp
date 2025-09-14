35350f7b0b7de3af22675f52fc9d2e5244705e37
Todor Milev <todor.milev@gmail.com>
2025-09-13 21:21:11 -0700

    New include should unbreak optimized gcc build.

--- src/calculator_expressions.cpp
+++ src/calculator_expressions.cpp
@@ -4,6 +4,7 @@
 #include "math_extra_semisimple_lie_algebras.h" // IWYU pragma: keep: breaks g++ -02 optimization build.
 #include "math_extra_symmetric_groups_and_generalizations.h"
 #include "math_general.h"
+#include "math_general_implementation.h" // IWYU pragma: keep: breaks g++ -02 optimization build.
 #include "math_general_polynomial_computations_basic_implementation.h" // IWYU pragma: keep: breaks g++ -02 optimization build.
 #include "math_rational_function.h"
 

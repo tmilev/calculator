491dc7431a0fdd686ee160a4e94358a1922eb363
Todor Milev <todor.milev@gmail.com>
2026-05-03 12:06:29 -0700

    Fix typo in calculator function comment

--- src/calculator_implementation_2.cpp
+++ src/calculator_implementation_2.cpp
@@ -1296,7 +1296,7 @@ bool Calculator::EvaluateLoop::userDefinedEvaluation() {
       std::stringstream substitutionComment;
       if (this->history != nullptr) {
         substitutionComment
-        << "User-defined substition: "
+        << "\text{User-defined substitution: }"
         << currentPattern.toString();
       }
       this->setOutput(afterPatternMatch, substitutionComment.str());
